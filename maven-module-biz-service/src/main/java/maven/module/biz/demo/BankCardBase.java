0d9ded80e43e6230f9c2f4e96e307e1a35b0eafd
unknown <wb-zp195009@hz.ali.com>
2016-09-29 15:01:16 +0800

    init framework

---
+++ maven-module-biz-service/src/main/java/maven/module/biz/demo/BankCardBase.java
@@ -0,0 +1,7 @@
+package maven.module.biz.demo;
+
+public class BankCardBase {
+	public void showCardType(){
+		System.out.println("I am ICBC card.");
+	}
+}
---
+++ maven-module-core-service/src/main/java/maven/module/demo/AccountBase.java
@@ -0,0 +1,7 @@
+package maven.module.demo;
+
+public class AccountBase {
+	public void showMoney(){
+		System.out.println("I have no money");
+	}
+}
